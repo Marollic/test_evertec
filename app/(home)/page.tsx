54d5411addb0887c7e234c278365803019a7e4d2
Djalma Mariano <djalmaoliveira@Djalmas-MacBook-Pro.local>
2024-07-24 12:16:49 -0300

    feat: add use callback

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
-"use client" 
+"use client"
 
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import Input from '@/components/input'
 import Button from '@/components/button'
 import Alert from '@/components/alert'
@@ -10,30 +10,27 @@ const LandingPage = () => {
   const [valorCapturado, setValorCapturado] = useState<string>('')
   const [mostrarAlerta, setMostrarAlerta] = useState<boolean>(false)
 
-  const cambioValorCapturado = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const cambioValorCapturado = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setValorCapturado(e.target.value)
     if (mostrarAlerta) setMostrarAlerta(false)
-  }
+  }, [setValorCapturado, mostrarAlerta, setMostrarAlerta])
 
-  const clickBoton = () => {
-    setMostrarAlerta(true)
-  }
+  const alteraEstado = useCallback(() => {
+    setMostrarAlerta(!mostrarAlerta)
+  }, [setMostrarAlerta, mostrarAlerta])
 
-  const cierrAlerta = () => {
-    setMostrarAlerta(false)
-  }
 
-  return ( 
+  return (
     <div className="h-full ">
-    <div className="container">
-      <Input value={valorCapturado} onChange={cambioValorCapturado} />
-      <Button onClick={clickBoton} disabled={!valorCapturado}>
-        Mostrar Alerta
-      </Button>
-      {mostrarAlerta && <Alert message={`Aviso de Alerta! | ${valorCapturado}`} onClose={cierrAlerta} />}
+      <div className="container">
+        <Input value={valorCapturado} onChange={cambioValorCapturado} />
+        <Button onClick={alteraEstado} disabled={!valorCapturado}>
+          Mostrar Alerta
+        </Button>
+        {mostrarAlerta && <Alert message={`Aviso de Alerta! | ${valorCapturado}`} onClose={alteraEstado} />}
+      </div>
     </div>
-    </div>
-   );
+  );
 }
- 
+
 export default LandingPage;
